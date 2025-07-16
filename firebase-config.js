1962186707946a9d9c9f0a376d0f8b009e073f21
Salvador Frisco <salvadorfrisco70@gmail.com>
2025-07-16 10:21:31 -0300

    first commit

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+
+const firebaseConfig = {
+  "apiKey": "AIzaSyA4fY_cvrYHgDhAIrBENE8PGpXXaSZue3A",
+  "authDomain": "frisco-auth.firebaseapp.com",
+  "projectId": "frisco-auth",
+  "storageBucket": "frisco-auth.firebasestorage.app",
+  "messagingSenderId": "881951716466",
+  "appId": "1:881951716466:web:792dc953c587671a203eeb",
+  "measurementId": "G-RTNZEELDJP"
+};
+
+firebase.initializeApp(firebaseConfig);
